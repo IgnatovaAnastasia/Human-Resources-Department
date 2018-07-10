76faf571b5990ab4d19450efc9ddb9a793b97e96
IgnatovaAnastasia <31617636+IgnatovaAnastasia@users.noreply.github.com>
2018-07-10 16:16:45 +0300

    start
    st

---
+++ Human Resources Department/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Human_Resources_Department.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
